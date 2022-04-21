1197954602d5cd4ce70c4159a50c6269d242f2e5
lazynormz <skupi1234@gmail.com>
2022-04-21 06:49:36 +0200

    added test for login cases

--- tests/ui/tests.js
+++ tests/ui/tests.js
@@ -2,6 +2,24 @@ import { Selector, ClientFunction } from 'testcafe'
 
 fixture("UI Tests")
 
+
+/***
+ * Function used to generate a random string of specified characters, used for creating new random emails for testing purpose
+ * @param length length of the random string wanted
+ * @returns a random string of desired length @see length
+ */
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+
 test.page("http://localhost:9888/admin/register")("Testing no input", async t => {
     await t
         .click(".btn")
@@ -24,12 +42,49 @@ test.page("http://localhost:9888/admin/register")("Testing no match password", a
 })
 
 test.page("http://localhost:9888/admin/register")("Testing redirect on success", async t => {
-    const getLocation = ClientFunction(() => document.location.href)
+    const getLocation = ClientFunction(() => document.location.href)    //Utility function
+
+    let randomEmail = "testing" + makeid(5) + "@test.com"
+
     await t
-        .typeText("#registerEmail", "testing@test.com")
+        .typeText("#registerEmail", randomEmail)
         .typeText("#registerName", "testing")
         .typeText("#registerPassword", "realPassword")
         .typeText("#registerConfirmPassword", "realPassword")
         .click(".btn")
         .expect(getLocation()).contains("http://localhost:9888/admin/login/?returnUrl=http%3A%2F%2Flocalhost%3A9888%2Fadmin")
+        .takeScreenshot()
+})
+
+
+
+
+
+test.page("http://localhost:9888/admin/login")("Testing email doesn't exist on login", async t => {
+    await t
+        .typeText("#loginEmail", "doesNotExist@nope.com")
+        .typeText("#loginPassword", "randomPass")
+        .click(".btn")
+        .expect(Selector(".account-message").innerText).eql("Login failed, please try again.")
+        .takeScreenshot()
+})
+
+test.page("http://localhost:9888/admin/login")("Testing password not correct on login", async t => {
+    await t
+        .typeText("#loginEmail", "testing@test.com")
+        .typeText("#loginPassword", "notTheCorrectPassword")
+        .click(".btn")
+        .expect(Selector(".account-message").innerText).eql("Login failed, please try again.")
+        .takeScreenshot()
+})
+
+test.page("http://localhost:9888/admin/login")("Testing redirect on successful login", async t => {
+    const getLocation = ClientFunction(() => document.location.href)    //Utility function
+
+    await t
+        .typeText("#loginEmail", "testing@test.com")
+        .typeText("#loginPassword", "realPassword")
+        .click(".btn")
+        .expect(getLocation()).contains("http://localhost:9888/admin/")
+        .takeScreenshot()
 })
