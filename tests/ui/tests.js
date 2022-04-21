1bde8802da6d8d14a637ca9ce0218e342f7d9131
lazynormz <skupi1234@gmail.com>
2022-04-21 09:56:11 +0200

    Formatting

--- tests/ui/tests.js
+++ tests/ui/tests.js
@@ -10,14 +10,13 @@ const TEST_URL = "185.51.76.42:9888"
  * @returns a random string of desired length
  */
 function makeid(length) {
-    var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < length; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * 
- charactersLength));
-   }
-   return result;
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
 }
 
 
